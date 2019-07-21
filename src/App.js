c3ae08542d9cbc382457383952ab5ccbb8cc80f0
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-21 21:20:05 +0200

    change props

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ class App extends Component {
     return (
       <div className="App">
         <Travel 
-          desination="Tokyo"
+          destination="Tokyo"
           country="Japan"
           photo="https://red-point.fr/wp-content/uploads/2017/02/tokyo-mud-bath-bar-mudbath0716.jpg"
           distance="9 710 km"
         />
         <Travel 
-          desination="New York"
+          destination="New York"
           country="USA"
           photo="https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg"
           distance="5 834 km"
